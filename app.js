cdda4345df2ce4412a5710dc594d5c4a0afff578
sharada-marasinha <sharadamarasinha@gmail.com>
2024-05-19 11:08:37 +0530

    nav links

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll('.nav-link');
-
+    
     navLinks.forEach(link => {
         link.addEventListener('click', function() {
             navLinks.forEach(link => link.classList.remove('active'));
