b6dfaa424409eea48ae418d6db449dda84bb320f
sharada-marasinha <sharadamarasinha@gmail.com>
2024-10-07 14:51:51 +0530

    404

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log(navigator.userAgent);
-    const navLinks = document.querySelectorAll('.nav-link');
-    
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            navLinks.forEach(link => link.classList.remove('active'));
-            this.classList.add('active');
-        });
-    });
-
-
-
 
