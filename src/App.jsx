af072589992335fc03431256964df521d4ecd32a
sharada-marasinha <sharadamarasinha@gmail.com>
2024-11-08 20:50:16 +0530

    samples

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import NavBar from "./assets/components/NavBar";
+import Hero from "./assets/components/Hero";
+import Footer from "./assets/components/Footer";
+import Project from "./assets/components/Project";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Project />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
