59dad9ff758d1e5de0f1be3fea61e20627db32af
sharada-marasinha <sharadamarasinha@gmail.com>
2024-11-12 17:08:28 +0530

    chage lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
+import flowbite from "flowbite-react/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content(), "./node_modules/flowbite/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [flowbite.plugin()],
+};
